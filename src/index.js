6efcbeb867688a29aaeb924e988d7f4904115953
Ariel Herman <aeh.herman@gmail.com>
2017-11-18 17:10:25 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// Done with Create React App
 
-// IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
-ReactDOM.render(
-  meInReact,
-  document.getElementById('global')
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const title = React.createElement("h1", {}, "An Awesome Person");
+const paragraph = React.createElement("p", {}, "Who is learning React");
+const list = React.createElement("ul", { className: "my-interests" }, [
+  React.createElement("li", { className: "brown" }, "JavaScript"),
+  React.createElement("li", { className: "pink" }, "React"),
+  React.createElement("li", { className: "yellow" }, "Movies"),
+  React.createElement("li", { className: "yellow" }, "Ice cream")
+]);
+
+const list2 = React.createElement(
+  "div",
+  {},
+  React.createElement("h1", {}, "My favorite ice cream flavors"),
+  React.createElement("ul", {}, [
+    React.createElement("li", {}, "Chocolate"),
+    React.createElement("li", {}, "Vanilla"),
+    React.createElement("li", {}, "Banana")
+  ])
 );
+const container = React.createElement("div", { className: "me" }, [
+  title,
+  paragraph,
+  listA
+]);
 
-export default meInReact
-*/
+class App extends Component {
+  render() {
+    return container;
+  }
+}
+
+export default App;
